40b2e169dbca83da44620bd68e20ea1b2f2a319b
yangqi3 <sloanyang@gmail.com>
2013-10-26 13:56:11 +0800

    Upgrade builder Fixing Rewinder bugs

--- Tools/WebkitHistoryRewinder/Checkout.sh
+++ Tools/WebkitHistoryRewinder/Checkout.sh
@@ -4,20 +4,36 @@ rootdir=$PWD
 
 if [ $1 = "gtk" ]
 then 
-	revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version.txt`
+	if [ -e $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt ]
+	then
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt`
+	else 
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version.txt`
+	fi
+
 	echo "Checking out to r$revision"
 	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
 	cd webkit
 	git checkout $git_revision
 	cd ..
-	echo $((revision+1)) > $PWD/Tools/WebkitHistoryRewinder/gtk-version.txt
+	echo $((revision+1)) > $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt
+
 elif [ $1 = "uc" ]
 then
-	revision=`cat uc-version.txt`
+	if [ -e $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt ]
+	then
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt`
+	else 
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/uc-version.txt`
+	fi
+
 	echo "Checking out to r$revision"
-	git_revision=$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit
+	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
+	cd webkit
 	git checkout $git_revision
-	echo $((revision+1)) > gtk-version.txt
+	cd ..
+	echo $((revision+1)) > $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt
+
 else
 	echo "Error: not supported platform"
 fi
