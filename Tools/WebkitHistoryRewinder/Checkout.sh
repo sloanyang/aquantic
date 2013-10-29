4fe9233bb252e408c1eff80b81f5ac12ebf6afea
yangqi3 <sloanyang@gmail.com>
2013-10-29 09:41:38 +0800

    Adding the stopping shell code for rewinder

--- Tools/WebkitHistoryRewinder/Checkout.sh
+++ Tools/WebkitHistoryRewinder/Checkout.sh
@@ -11,6 +11,8 @@ then
 		revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version.txt`
 	fi
 
+	stop_revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version-end.txt`
+
 	if [ ! -d $PWD/webkit/WebKitBuild ]
 	then
 		mkdir $PWD/webkit/WebKitBuild
@@ -19,9 +21,16 @@ then
 	echo "Checking out to r$revision"
 	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
 	cd webkit
-	git checkout $git_revision
-	cd ..
-	echo $((revision+1)) > $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt
+
+	if [ $((revision+1)) -lt $((stop_revision)) ] 
+	then 
+		git checkout $git_revision
+		cd ..
+		echo $((revision+1)) > $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt
+	else
+		cd ..
+		exit 2
+	fi
 
 elif [ $1 = "uc" ]
 then
@@ -32,6 +41,8 @@ then
 		revision=`cat $PWD/Tools/WebkitHistoryRewinder/uc-version.txt`
 	fi
 
+	stop_revision=`cat $PWD/Tools/WebkitHistoryRewinder/uc-version-end.txt`
+
 	if [ ! -d $PWD/webkit/WebKitBuild ]
 	then
 		mkdir $PWD/webkit/WebKitBuild
@@ -40,9 +51,17 @@ then
 	echo "Checking out to r$revision"
 	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
 	cd webkit
-	git checkout $git_revision
-	cd ..
-	echo $((revision+1)) > $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt
+
+	if [ $((revision+1)) -lt $((stop_revision)) ] 
+	then 
+		git checkout $git_revision
+		cd ..
+		echo $((revision+1)) > $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt
+	else
+		cd ..
+		exit 2
+	fi
+
 
 else
 	echo "Error: not supported platform"
