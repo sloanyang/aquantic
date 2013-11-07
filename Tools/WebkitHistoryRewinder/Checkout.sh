9e069bf8f16c12ef5238d99214c39d39e8c477d7
yangqi3 <sloanyang@gmail.com>
2013-11-07 18:39:00 +0800

    Add rewinder for qt build

--- Tools/WebkitHistoryRewinder/Checkout.sh
+++ Tools/WebkitHistoryRewinder/Checkout.sh
@@ -62,6 +62,36 @@ then
 		exit 2
 	fi
 
+elif [ $1 = "qt" ]
+then
+	if [ -e $PWD/Tools/WebkitHistoryRewinder/qt-version-local.txt ]
+	then
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/qt-version-local.txt`
+	else 
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/qt-version.txt`
+	fi
+
+	stop_revision=`cat $PWD/Tools/WebkitHistoryRewinder/qt-version-end.txt`
+
+	if [ ! -d $PWD/webkit/WebKitBuild ]
+	then
+		mkdir $PWD/webkit/WebKitBuild
+	fi
+
+	echo "Checking out to r$revision"
+	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
+	cd webkit
+
+	if [ $((revision)) -lt $((stop_revision)) ] 
+	then 
+		git checkout -f $git_revision
+		cd ..
+		echo $((revision+100)) > $PWD/Tools/WebkitHistoryRewinder/qt-version-local.txt
+	else
+		cd ..
+		exit 2
+	fi
+
 
 else
 	echo "Error: not supported platform"
