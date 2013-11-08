f9425456142ec5c25f00c23b86cc4ee63ecae213
yangqi3 <sloanyang@gmail.com>
2013-11-08 17:22:40 +0800

    Add new non patching checkout script

---
+++ Tools/WebkitHistoryRewinder/Checkout-simple.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+rootdir=$PWD
+
+if [ $1 = "gtk" ]
+then 
+	if [ -e $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt ]
+	then
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt`
+	else 
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version.txt`
+	fi
+
+	stop_revision=`cat $PWD/Tools/WebkitHistoryRewinder/gtk-version-end.txt`
+
+	if [ ! -d $PWD/webkit/WebKitBuild ]
+	then
+		mkdir $PWD/webkit/WebKitBuild
+	fi
+
+	echo "Checking out to r$revision"
+	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
+	cd webkit
+
+	if [ $((revision)) -lt $((stop_revision)) ] 
+	then 
+		git checkout -f $git_revision
+		cd ..
+		echo $((revision+100)) > $PWD/Tools/WebkitHistoryRewinder/gtk-version-local.txt
+	else
+		cd ..
+		exit 2
+	fi
+
+elif [ $1 = "uc" ]
+then
+	if [ -e $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt ]
+	then
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt`
+	else 
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/uc-version.txt`
+	fi
+
+	stop_revision=`cat $PWD/Tools/WebkitHistoryRewinder/uc-version-end.txt`
+
+	if [ ! -d $PWD/webkit/WebKitBuild ]
+	then
+		mkdir $PWD/webkit/WebKitBuild
+	fi
+
+	echo "Checking out to r$revision"
+	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
+	cd webkit
+
+	if [ $((revision)) -lt $((stop_revision)) ] 
+	then 
+		git checkout -f $git_revision
+		cd ..
+		echo $((revision+100)) > $PWD/Tools/WebkitHistoryRewinder/uc-version-local.txt
+	else
+		cd ..
+		exit 2
+	fi
+
+elif [ $1 = "qt" ]
+then
+	if [ -e $PWD/Tools/WebkitHistoryRewinder/qt-version-local.txt ]
+	then
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/qt-version-local.txt`
+	else 
+		revision=`cat $PWD/Tools/WebkitHistoryRewinder/qt-version.txt`
+	fi
+
+	stop_revision=`cat $PWD/Tools/WebkitHistoryRewinder/qt-version-end.txt`
+
+	if [ ! -d $PWD/webkit/WebKitBuild ]
+	then
+		mkdir $PWD/webkit/WebKitBuild
+	fi
+
+	echo "Checking out to r$revision"
+	git_revision=`$rootdir/Tools/Scripts/git-svn.sh find-rev $revision $rootdir/webkit`
+	cd webkit
+
+	if [ $((revision)) -lt $((stop_revision)) ] 
+	then 
+		git checkout -f $git_revision
+		cd ..
+		echo $((revision+100)) > $PWD/Tools/WebkitHistoryRewinder/qt-version-local.txt
+	else
+		cd ..
+		exit 2
+	fi
+
+
+else
+	echo "Error: not supported platform"
+fi
+
+
